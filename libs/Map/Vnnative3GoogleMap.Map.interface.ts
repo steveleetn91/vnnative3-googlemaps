f38076cb19df6a1e3a8ca48b1621ff8669f0e691
CEO <hoang.le.tn91@gmail.com>
2022-03-14 17:40:01 +0700

    Update core

--- libs/Map/Vnnative3GoogleMap.Map.interface.ts
+++ libs/Map/Vnnative3GoogleMap.Map.interface.ts
@@ -1,8 +1,12 @@
+export interface Vnnative3GoogleMapInitGetMapoption {
+    'camera': {
+        target: {
+            lat : number,
+            lng : number
+        },
+        zoom: number
+      }
+}
 export default interface Vnnative3GoogleMapInitInterface {
-    getMap(div : HTMLElement,options : {
-        'camera': {
-            target: number,
-            zoom: number
-          }
-    }) : any
+    getMap(div : HTMLElement,options : Vnnative3GoogleMapInitGetMapoption) : any
 }   
--- libs/Map/Vnnative3GoogleMap.Map.ts
+++ libs/Map/Vnnative3GoogleMap.Map.ts
@@ -1,9 +1,9 @@
-import Vnnative3GoogleMapInitInterface from "./Vnnative3GoogleMap.Map.interface";
+import Vnnative3GoogleMapInitInterface, { Vnnative3GoogleMapInitGetMapoption } from "./Vnnative3GoogleMap.Map.interface";
 import Vnnative3GoogleMapMarker from "./Vnnative3GoogleMap.Marker";
 
 export default class Vnnative3GoogleMapInit implements Vnnative3GoogleMapInitInterface{
     plugin: any = window; 
-    getMap(div: HTMLElement, options: { camera: { target: number; zoom: number; }; }): Vnnative3GoogleMapMarker {
+    getMap(div: HTMLElement, options: Vnnative3GoogleMapInitGetMapoption): Vnnative3GoogleMapMarker {
         const map = this.plugin.plugin.google.maps.Map.getMap(div, options);
         return new Vnnative3GoogleMapMarker(map);
     }
